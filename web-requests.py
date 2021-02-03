3b1112cd11d74561759e39e381c6060bc9657595
MrAndyStorey <77452018+MrAndyStorey@users.noreply.github.com>
2021-02-03 08:57:57 +0000

    Update web-requests.py

--- web-requests.py
+++ web-requests.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 
 import requests as req
+
+# Regex Pattern will return full match including the html tags and group 1 () as the contents
+regex_pattern = r"\<title\>(.+?)\<\/title\>"
+
+# URL to check
+url = "https://httpd.apache.org/"
